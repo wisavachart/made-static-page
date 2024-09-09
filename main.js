b2c7599571aa24d39d03609e9b5b26e715ec25d0
WIsavachart <wisavachart.s@gmail.com>
2024-09-09 15:43:34 +0700

    Finish md Channel mobile

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ import MadeChannelApp from "./src/madeChannelSection";
 //CHECK PATH
 // const currentPath = window.location.pathname;
 initialApp();
-
 function initialApp() {
+  adjustVH();
   new Slider();
   new NavBar();
 }
+
+function adjustVH() {
+  document.documentElement.style.setProperty(
+    "--vh",
+    `${window.innerHeight * 0.95}px`
+  );
+}
+window.addEventListener("resize", adjustVH);
