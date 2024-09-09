549e39f6de26827364efffb28bb9466974bd72dc
WIsavachart <wisavachart.s@gmail.com>
2024-09-09 09:27:41 +0700

    Finist sec 1

--- src/slider.js
+++ src/slider.js
@@ -3,7 +3,8 @@ class Slider {
     this.navigate_slider_prev = document.getElementById("navigate-slider-prev");
     this.navigate_slider_next = document.getElementById("navigate-slider-next");
     this._homePagePinPostHighlightSlider();
-    this._recentPostSocialsNewSwiper();
+    this._recentPostDesktopSocialsNewSwiper();
+    this._recentPostMobileSocialsNewSwiper();
     this.addEvent();
   }
 
@@ -31,11 +32,11 @@ class Slider {
       slidesPerView: "auto",
       centeredSlides: true,
       loop: true,
-      spaceBetween: 14,
-      autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
+      spaceBetween: 8,
+      // autoplay: {
+      //   delay: 3500,
+      //   disableOnInteraction: false,
+      // },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -46,33 +47,23 @@ class Slider {
       },
     });
   }
-  _recentPostSocialsNewSwiper() {
+  _recentPostMobileSocialsNewSwiper() {
     new Swiper(".recentSwiper", {
       slidesPerView: 2,
       spaceBetween: 24,
     });
   }
-  // _recentPostSlider() {
-  //   new Swiper(".recentSwiper", {
-  //     slidesPerView: 2,
-  //     loop: false,
-  //     spaceBetween: 10,
-  //     pagination: {
-  //       el: ".swiper-pagination-recent",
-  //       clickable: true,
-  //     },
-  //     breakpoints: {
-  //       773: {
-  //         slidesPerView: 2,
-  //         spaceBetween: 30,
-  //       },
-  //       817: {
-  //         slidesPerView: 3,
-  //         spaceBetween: 30,
-  //       },
-  //     },
-  //   });
-  // }
+  _recentPostDesktopSocialsNewSwiper() {
+    new Swiper(".recentSwiperDesktop", {
+      slidesPerView: 3,
+      spaceBetween: 16,
+      loop: false,
+      pagination: {
+        el: ".swiper-pagination-recent",
+        clickable: true,
+      },
+    });
+  }
 }
 
 export default Slider;
